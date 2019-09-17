b5cae90af60b64579f6b9d85506eb80e7829e62f
Daniel Hajduk <daniel.0@gmx.at>
2019-09-17 11:39:44 +0200

    Created new Projects

---
+++ NetChat.console2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetChat.console2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
