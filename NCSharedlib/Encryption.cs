6a4ad1dc0e2f8294165ef4c39227206fe42c0053
Livio Tavakolian <tavakolian.l02@htl-ottakring.ac.at>
2019-10-22 10:22:24 +0200

    symetric encryption finished

--- NCSharedlib/Encryption.cs
+++ NCSharedlib/Encryption.cs
@@ -8,14 +8,21 @@ namespace NCSharedlib
 {
     class Encryption
     {
-        public string encrypting(FileStream stream, string inputtext) {
+        
+        public byte[] encrypting(byte[]data, string inputtext) {
             DESCryptoServiceProvider cryptic = new DESCryptoServiceProvider();
+            MemoryStream memstream = new MemoryStream();
+            //byte[] iv = new byte[32];
 
             cryptic.Key = ASCIIEncoding.ASCII.GetBytes(inputtext);
-            cryptic.IV = ASCIIEncoding.ASCII.GetBytes(inputtext);
+            //cryptic.IV = iv;
 
-            CryptoStream crStream = new CryptoStream(stream,cryptic.CreateEncryptor(), CryptoStreamMode.Write);
-            return crStream.ToString();
+            CryptoStream crStream = new CryptoStream(memstream,cryptic.CreateEncryptor(), CryptoStreamMode.Write);
+            crStream.Write(data);
+            crStream.Close();
+
+            return memstream.ToArray();
+
         }
     }
 }
