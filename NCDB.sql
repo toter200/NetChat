577fd96574f387013c6fd0e88ac9549362ca9fa8
meikev3 <47141862+meikev3@users.noreply.github.com>
2019-10-22 10:00:26 +0200

    Add files via upload

---
+++ NCDB.sql
@@ -0,0 +1,78 @@
+-- MySQL dump 10.16  Distrib 10.1.37-MariaDB, for Win32 (AMD64)
+--
+-- Host: localhost    Database: NCDB
+-- ------------------------------------------------------
+-- Server version	10.1.37-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `dev`
+--
+
+DROP TABLE IF EXISTS `dev`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dev` (
+  `userID` int(6) NOT NULL,
+  `devID` int(6) NOT NULL AUTO_INCREMENT,
+  `ipAddress` varchar(20) NOT NULL,
+  PRIMARY KEY (`devID`),
+  KEY `fk` (`userID`),
+  CONSTRAINT `fk` FOREIGN KEY (`userID`) REFERENCES `usr` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dev`
+--
+
+LOCK TABLES `dev` WRITE;
+/*!40000 ALTER TABLE `dev` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dev` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usr`
+--
+
+DROP TABLE IF EXISTS `usr`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usr` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `mail` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `status` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usr`
+--
+
+LOCK TABLES `usr` WRITE;
+/*!40000 ALTER TABLE `usr` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usr` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-10-22  9:56:37
