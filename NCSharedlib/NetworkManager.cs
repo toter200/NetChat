9b7e64d298f7a4038bfc8730f08dd03dd36783bc
Livio Tavakolian <tavakolian.l02@htl-ottakring.ac.at>
2019-10-29 12:51:49 +0100

    Finished Send and Recieve Files + Half Unit Test

--- NCSharedlib/NetworkManager.cs
+++ NCSharedlib/NetworkManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -44,7 +45,40 @@ namespace NCSharedlib
             tcpSendingThread.Start();
         }
 
+        private static void ReadFile(string path, TcpClient socket)
+        {
+            const int bufsize = 8192;
 
+            var buffer = new byte[bufsize];
+            NetworkStream ns = socket.GetStream();
+
+            using (var s = File.OpenRead(path))
+            {
+                int actuallyRead;
+                while ((actuallyRead = s.Read(buffer, 0, bufsize)) > 0)
+                {
+                    ns.Write(buffer, 0, actuallyRead);
+                }
+            }
+            ns.Flush();
+        }
+
+        private static void SendFile(IPAddress address, int port, string path)
+        {
+            var client = new TcpClient();
+            client.Connect(new IPEndPoint(address, port));
+            try
+            {
+                NetworkStream networkStream = client.GetStream();
+                FileStream fileStream = File.OpenRead(path);
+                fileStream.CopyTo(networkStream);
+                fileStream.Close();
+            }
+            finally
+            {
+                client?.Close();
+            }
+        }
     }
     
     
