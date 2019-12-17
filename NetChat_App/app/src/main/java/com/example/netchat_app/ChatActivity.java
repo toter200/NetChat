cbe8c1ded50dec9632813f13062fa6f95c8af24c
Livio Tavakolian <tavakolian.l02@htl-ottakring.ac.at>
2019-12-17 10:19:16 +0100

    Finished-Chat-Layout

--- NetChat_App/app/src/main/java/com/example/netchat_app/ChatActivity.java
+++ NetChat_App/app/src/main/java/com/example/netchat_app/ChatActivity.java
@@ -1,9 +1,16 @@
 package com.example.netchat_app;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -14,7 +21,7 @@ public class ChatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-
+        Log.d("myTag", "This is my message");
 
     }
 
@@ -23,4 +30,20 @@ public class ChatActivity extends AppCompatActivity {
         startActivity(intent1);
     }
 
+}
+
+class TCPClient {
+    public static void main(String argv[]) throws Exception {
+        String sentence;
+        String modifiedSentence;
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        Socket clientSocket = new Socket("localhost", 48000);
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        sentence = inFromUser.readLine();
+        outToServer.writeBytes(sentence + 'n');
+        modifiedSentence = inFromServer.readLine();
+        System.out.println("FROM SERVER: " + modifiedSentence);
+        clientSocket.close();
+    }
 }
